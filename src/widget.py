108effa7b737b0f6a3681bddd213d1a8bf8e249a
Vadim <dolinenko2006@ya.ru>
2025-07-18 08:36:17 +0500

    widget

---
+++ src/widget.py
@@ -0,0 +1,9 @@
+from typing import Union
+
+def mask_account_card(a: Union[str, int], b: Union[str, int]) -> str:
+    """
+    Возвращать строку с замаскированным номером.
+    Visa Platinum 7000792289606361  - входной аргумент
+    Visa Platinum 7000 79** **** 6361  - выход функции
+    """
+    pass
