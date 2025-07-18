679b77ab317b47d0c9f4c44bd7eada6f48aeb557
Vadim <dolinenko2006@ya.ru>
2025-07-18 14:33:10 +0500

    mid change

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
 print(get_mask_card_number(1111111111111111)) # 16-ти значное число
 print(get_mask_account(22222222222222222222)) # 20-ти значное число
+print(mask_account_card())
+print(get_date())
