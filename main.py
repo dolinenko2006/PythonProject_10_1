b619c84ffc50e90f62633bf0ad9eaf5a0ff45374
Vadim <dolinenko2006@ya.ru>
2025-07-19 16:05:35 +0500

    finish

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from src.widget import get_date, mask_account_card
 
 print(get_mask_card_number(1111111111111111))  # 16-ти значное число
 print(get_mask_account(22222222222222222222))  # 20-ти значное число
-print(mask_account_card('Maestro 1111111111111111'))
-print(get_date('2222-22-22'))
+print(mask_account_card("Visa 1111111111111111"))
+print(get_date("2222-22-22"))
--- src/widget.py
+++ src/widget.py
@@ -1,31 +1,32 @@
 from typing import Union
 
+from src.masks import get_mask_account, get_mask_card_number
 
-def mask_account_card(arg: Union[str] = "Visa 0000000000000000") -> str:
+
+def mask_account_card(attribute: str = "Visa 0000000000000000") -> str:
     """
     Возвращать строку с замаскированным номером.
     Visa Platinum 7000792289606361  - входной аргумент
     Visa Platinum 7000 79** **** 6361  - выход функции
     """
-    a = str(arg)
-    str_arg_ = a.split(" ")  # ['Счет', '73654108430135874305']
-    number = str_arg_[-1]
-    words = str_arg_[0:-1]
-    if str_arg_[0] == "Счет" or str_arg_[0] == "Счёт":
-        return f"Счет **{number[-4:]}"
+    attribute = str(attribute)
+    list_attribute = attribute.split(" ")  # ['Счет', '73654108430135874305']
+    number = list_attribute[-1]
+    words = " ".join(list_attribute[0:-1])
+
+    if list_attribute[0] == "Счет" or list_attribute[0] == "Счёт":
+        return f'{words} {get_mask_account(number)}'
     else:
-        number_total = number[0:4] + " " + number[5:7] + "**" + " " + "****" + " " + number[-4:]
-        words_total = " ".join(words)
-        return f"{words_total} {number_total}"
+        return f'{words} {get_mask_card_number(number)}'
 
 
-def get_date(arg: Union[str] = "0000-00-00T00:00:00.00000") -> str:
+def get_date(date_: str = "0000-00-00T00:00:00.00000") -> str:
     """
     принимает на вход строку с датой
     "2024-03-11T02:26:18.671407" - входной аргумент
     "11.03.2024" - выход функции
     """
-    arg = str(arg)
-    date = arg[:10]  # 2024-03-11
-    year, month, day = tuple(date.split("-"))  # -> ('2024', '03', '11')
+    date_ = str(date_)
+    date_ = date_[:10]  # 2024-03-11
+    year, month, day = tuple(date_.split("-"))  # -> ('2024', '03', '11')
     return f"{day}.{month}.{year}"
