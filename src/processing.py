a1e6d2ee65489b1cb0ca12dea79edb28f3a768d2
Vadim <dolinenko2006@ya.ru>
2025-07-24 21:32:15 +0500

    fixe

---
+++ src/processing.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+
+def filter_by_state(a:list, b:str = 'EXECUTED') -> list:
+    """принимает список словарей и опционально значение для ключа state
+    (по умолчанию 'EXECUTED')"""
+    pass
+
+
+def sort_by_date(a:list, b:Optional[bool] = True) -> list:
+    """принимает список словарей и необязательный параметр.
+    Функция должна возвращать новый список, отсортированный по дате (date)."""
+    pass
