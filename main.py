09e7e906d3025b9435f7fca3ef4374b3941a7729
Vadim <dolinenko2006@ya.ru>
2025-07-25 20:20:51 +0500

    finished commit

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from src.processing import filter_by_state, sort_by_date
 # print(mask_account_card("Visa 1111111111111111"))
 # print(get_date("2222-22-22"))
 
-a = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-print(filter_by_state(a))
-print(sort_by_date(a, ))
+list_of_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+print(filter_by_state(list_of_dict))
+print(sort_by_date(list_of_dict))
--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
-def filter_by_state(list_of_dict: list, value_: str = "EXECUTED") -> list:
+from typing import Any
+
+
+def filter_by_state(list_of_dict: list[dict[str, Any]], value_: str = "EXECUTED") -> list[dict[str, Any]]:
     """принимает список словарей и опционально значение для ключа state
     (по умолчанию 'EXECUTED')"""
     new_list_of_dict = []  # список словарей, где все ключи 'state' имеют значение value_
@@ -9,18 +12,10 @@ def filter_by_state(list_of_dict: list, value_: str = "EXECUTED") -> list:
     return new_list_of_dict
 
 
-def sort_by_date(list_of_dict: list, descending: bool = True) -> list:
+def sort_by_date(list_of_dict: list[dict[str, Any]], descending: bool = True) -> list[dict[str, Any]]:
     """принимает список словарей и необязательный параметр.
     Функция должна возвращать новый список, отсортированный по дате (date).
     (сортировка по убыванию, т.е. сначала самые последние операции)"""
-    for dict_ in list_of_dict:
-        date_str = dict_["date"][:10].replace("-", "")  # '2018-06-30T02:08:58.425572' -> '20180630'
-        date_int = int(date_str)  # строка '20180630' -> число 20180630
-        dict_["date_int"] = date_int  # помещаем в словарь ключ ('date_int') = значение (число 20180630)
-
-    sorted_list = sorted(list_of_dict, key=lambda i: i["date_int"], reverse=descending)
-
-    for dict_ in sorted_list:
-        del dict_["date_int"]
+    sorted_list = sorted(list_of_dict, key=lambda i: i["date"], reverse=descending)
 
     return sorted_list
