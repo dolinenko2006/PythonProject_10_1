d7710661c735cafe3f3a8bb32b2a447275b5d931
Vadim <dolinenko2006@ya.ru>
2025-07-29 22:26:28 +0500

    first commit homework_10_2

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ print(get_mask_account('22222222222222222222'))  # 20-ти значное чис
 list_of_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 list_of_dict_2 = [{'id': 1, 'state': 'EMPTY-1', 'date': '2018-10-14'}, {'id': 2, 'state': 'EMPTY-2', 'date': '2018-10-14'}, {'id': 3, 'state': 'EMPTY-3', 'date': '2018-10-14'}]
 print(filter_by_state(list_of_dict))
-print(sort_by_date(list_of_dict))
+print(sort_by_date(list_of_dict, False))
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -18,7 +18,6 @@ def test_filter_by_state():
                                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
                                      ]
                              ),
-
                              (
                                      list_of_dict,
                                      'EXECUTED',
@@ -30,16 +29,12 @@ def test_filter_by_state():
                              (
                                      list_of_dict,
                                      'EMPTY-1',
-                                     [
-
-                                     ]
+                                     []
                              ),
-(
+                             (
                                      list_of_dict,
                                      None,
-                                     [
-
-                                     ]
+                                     []
                              )
                         ]
                         )
@@ -49,4 +44,21 @@ def test_filter_by_state(list_of_dict, state, result):
 
 def test_sort_by_date():
     assert sort_by_date([]) == []
-    assert sort_by_date(list_of_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+    assert (sort_by_date(list_of_dict,True) ==
+            [
+                {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+            ]
+            )
+    assert (sort_by_date(list_of_dict,False) ==
+            [
+                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+             ]
+            )
+
+
