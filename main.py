96617a2e8a2581ecbd17ca942ab394838cb0f3ab
Vadim <dolinenko2006@ya.ru>
2025-08-17 22:28:04 +0500

    finish commit 11,1

--- main.py
+++ main.py
@@ -1,51 +1,66 @@
-from src.masks import get_mask_account, get_mask_card_number
-from src.widget import get_date, mask_account_card
-from src.processing import filter_by_state, sort_by_date
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 import ast
 
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
+from src.masks import get_mask_account, get_mask_card_number
+from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
+
 print(get_mask_card_number())  # 16-ти значное число
-print(get_mask_account('22222222222222222222'))  # 20-ти значное число
+print(get_mask_account("22222222222222222222"))  # 20-ти значное число
 print(mask_account_card("Visa 1111111111111111"))
 print(mask_account_card("Счёт 11111111111111111111"))  # == "Visa 1111 11** **** 1111"
 print(get_date("2024-03-11"))
 
-list_of_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-list_of_dict_2 = [{'id': 1, 'state': 'EMPTY-1', 'date': '2018-10-14'}, {'id': 2, 'state': 'EMPTY-2', 'date': '2018-10-14'}, {'id': 3, 'state': 'EMPTY-3', 'date': '2018-10-14'}]
+list_of_dict = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+list_of_dict_2 = [
+    {"id": 1, "state": "EMPTY-1", "date": "2018-10-14"},
+    {"id": 2, "state": "EMPTY-2", "date": "2018-10-14"},
+    {"id": 3, "state": "EMPTY-3", "date": "2018-10-14"},
+]
 print(filter_by_state(list_of_dict))
 print(sort_by_date(list_of_dict))
-print('\n' + '=' * 10 + '\n')  # визуальный разделитель
+print("\n" + "=" * 10 + "\n")  # визуальный разделитель
 
-list_of_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+list_of_dict = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 print(filter_by_state(list_of_dict))
 print(sort_by_date(list_of_dict))
-print('\n' + '=' * 10 + '\n')  # визуальный разделитель
+print("\n" + "=" * 10 + "\n")  # визуальный разделитель
 
 
-with open("transactions.txt", "r", encoding="utf-8") as f: # читаем список словарей из текстового документа
+with open("transactions.txt", "r", encoding="utf-8") as f:  # читаем список словарей из текстового документа
     data = f.read()
 transactions = ast.literal_eval(data)
 
 
-list_of_codes = ['USD', 'RUB']  # список валют в транзакциях
-code = 'US'
-if code not in list_of_codes:  # проверяем првильность выбора валюты
-    print(f'Выберите валюту {", ".join(list_of_codes)}')
-else:
-    usd_transactions = filter_by_currency(transactions, code)  # запускаем генератор с отфильтрованными словарями
-    for _ in range(4):
-        try:
-            print(next(usd_transactions))
-        except StopIteration:
-            print("Список транзакций закончился")  # выводим сообщение об окончании итераций
-print('\n' + '=' * 10 + '\n')  # визуальный разделитель
+list_of_codes = ["USD", "RUB"]
+usd_transactions = filter_by_currency(transactions, "RUB")  # запускаем генератор с отфильтрованными словарями
+for _ in range(4):
+    try:
+        print(next(usd_transactions))
+    except ValueError:
+        print(f"Ошибка выбора валюты: {', '.join(list_of_codes)}")
+        break
+    except StopIteration:
+        print("Список закончился")  # выводим сообщение об окончании итераций
+        break
+print("\n" + "=" * 10 + "\n")  # визуальный разделитель
 
 
 descriptions = transaction_descriptions(transactions)  # запускаем генератор с информацией о транзакциях
 for i in descriptions:
     print(i)
-print('\n' + '=' * 10 + '\n')  # визуальный разделитель
+print("\n" + "=" * 10 + "\n")  # визуальный разделитель
 
 
-for card_number in card_number_generator(1, 5):  # запускаем генератор рандомных номеров карт
+for card_number in card_number_generator(1, 10):  # запускаем генератор рандомных номеров карт
     print(card_number)
--- src/generators.py
+++ src/generators.py
@@ -1,111 +1,29 @@
-import ast
 import random
 
+list_of_codes = ["USD", "RUB"]
 
-# with open("../transactions.txt", "r", encoding="utf-8") as f:
-#     data = f.read()
-# transactions = ast.literal_eval(data)
 
-# transactions = (
-#     [
-#         {
-#             "id": 939719570,
-#             "state": "EXECUTED",
-#             "date": "2018-06-30T02:08:58.425572",
-#             "operationAmount": {
-#                 "amount": "9824.07",
-#                 "currency": {
-#                     "name": "USD",
-#                     "code": "USD"
-#                 }
-#             },
-#             "description": "Перевод организации",
-#             "from": "Счет 75106830613657916952",
-#             "to": "Счет 11776614605963066702"
-#         },
-#         {
-#             "id": 142264268,
-#             "state": "EXECUTED",
-#             "date": "2019-04-04T23:20:05.206878",
-#             "operationAmount": {
-#                 "amount": "79114.93",
-#                 "currency": {
-#                     "name": "USD",
-#                     "code": "USD"
-#                 }
-#             },
-#             "description": "Перевод со счета на счет",
-#             "from": "Счет 19708645243227258542",
-#             "to": "Счет 75651667383060284188"
-#         },
-#         {
-#             "id": 873106923,
-#             "state": "EXECUTED",
-#             "date": "2019-03-23T01:09:46.296404",
-#             "operationAmount": {
-#                 "amount": "43318.34",
-#                 "currency": {
-#                     "name": "руб.",
-#                     "code": "RUB"
-#                 }
-#             },
-#             "description": "Перевод со счета на счет",
-#             "from": "Счет 44812258784861134719",
-#             "to": "Счет 74489636417521191160"
-#         },
-#         {
-#             "id": 895315941,
-#             "state": "EXECUTED",
-#             "date": "2018-08-19T04:27:37.904916",
-#             "operationAmount": {
-#                 "amount": "56883.54",
-#                 "currency": {
-#                     "name": "USD",
-#                     "code": "USD"
-#                 }
-#             },
-#             "description": "Перевод с карты на карту",
-#             "from": "Visa Classic 6831982476737658",
-#             "to": "Visa Platinum 8990922113665229"
-#         },
-#         {
-#             "id": 594226727,
-#             "state": "CANCELED",
-#             "date": "2018-09-12T21:27:25.241689",
-#             "operationAmount": {
-#                 "amount": "67314.70",
-#                 "currency": {
-#                     "name": "руб.",
-#                     "code": "RUB"
-#                 }
-#             },
-#             "description": "Перевод организации",
-#             "from": "Visa Platinum 1246377376343588",
-#             "to": "Счет 14211924144426031657"
-#         }
-#     ]
-# )
-#print(transactions)
-
-def filter_by_currency(my_list,code='USD'):
+def filter_by_currency(my_list, code="USD"):
     # принимает на вход список словарей, представляющих транзакции, где валюта операции соответствует заданной
+    if code not in list_of_codes:
+        raise ValueError(f'Выберите валюту: {", ".join(list_of_codes)}')
 
     for i in my_list:
-        if i['operationAmount']['currency']['code'] == code:
+        if i["operationAmount"]["currency"]["code"] == code:
             yield i
 
 
 def transaction_descriptions(my_list):
     # принимает список словарей с транзакциями и возвращает описание каждой операции по очереди
     for i in my_list:
-        yield i['description']
+        yield i["description"]
 
 
-def card_number_generator(start=0, stop=9999999999999999):
+def card_number_generator(start=1, stop=5):
     # выдает номера банковских карт в формате XXXX XXXX XXXX XXXX
     random_numbers = set()
     while True:
-        number = random.randint(start,stop)
+        number = random.randint(start, stop)
         if number in random_numbers:  # проверяем чтобы не было повторений номеров карт
             continue
 
